27d5544fd184479f6def606e15deb9c42551cf45
hoangnm <hoangnm.ndm@gmail.com>
2023-03-04 21:28:45 +0700

    add btvn

---
+++ lession_10_DOM_Events/bt.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Thực hiện validator cho giao diện login form đã học ở nội dung CSS như sau:
+ * - email  đổi thành username và có độ dài tối thiểu là 8 ký tự
+ * - username không được bắt đầu bằng một số.
+ * - password có độ dài tối thiểu 8 ký tự
+ *
+ *
+ * 2. Code slide show dựa vào ví dụ của W3C và làm với slide show có 5 item
+ *
+ * 3. Code 1 web app có tính năng +, - , *, /.
+ */
